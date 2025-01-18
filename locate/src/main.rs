39690d2aadaac4771abd8cf37f9931542480d24a
rip_super <150400664+rip-super@users.noreply.github.com>
2025-01-18 11:51:25 -0800

    Add files via upload

---
+++ locate/src/main.rs
@@ -0,0 +1,161 @@
+use regex::Regex;
+use std::env;
+use std::io;
+use std::process;
+use walkdir::WalkDir;
+
+struct Options {
+    base_name: bool,
+    case_sens: bool,
+    count: bool,
+    limit: u32,
+    regex: Option<Regex>,
+}
+
+impl Options {
+    fn new() -> Self {
+        Options {
+            base_name: false,
+            case_sens: false,
+            count: false,
+            limit: u32::MAX,
+            regex: None,
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut options = Options::new();
+    let file_name = env::current_exe()?
+        .file_stem()
+        .unwrap()
+        .to_string_lossy()
+        .into_owned();
+
+    check_args(&args, &file_name, &mut options);
+
+    let search_term = &args[args.len() - 1];
+    find_file(search_term, &options);
+
+    Ok(())
+}
+
+fn check_args(args: &[String], file_name: &str, options: &mut Options) {
+    if args.len() == 1 || ((args[1] == "/l" || args[1] == "--limit") && args.len() > 4) {
+        eprintln!("Usage: {}[.exe] [args] <search_term>", file_name);
+        eprintln!("enter '{} --help' to learn more", file_name);
+        process::exit(1);
+    }
+
+    let arg = &args[1];
+
+    if arg == "/h" || arg == "--help" {
+        println!("Usage: {}[.exe] [args] <search_term>", file_name);
+        println!("\nOPTIONAL ARGUMENTS\n");
+        println!("Note: Only one argument can be used at a time\n");
+        println!("/h or --help              Displays this help message");
+        println!("/b or --basename          Searches for files using their basename instead of their full path (case-insenitive)");
+        println!("/s or --case-sensitive    Searches for files using case-sensitive search");
+        println!("/c or --count             Only displays the number of matches and not the files matched");
+        println!("/l or --limit <number>    Limits the number of results displayed");
+        println!("/r or --regex <regexp>    Searches for files based on a regular expression");
+
+        process::exit(0)
+    } else if arg == "/b" || arg == "--basename" {
+        options.base_name = true;
+    } else if arg == "/s" || arg == "--case-sensitive" {
+        options.case_sens = true;
+    } else if arg == "/c" || arg == "--count" {
+        options.count = true;
+    } else if arg == "/l" || arg == "--limit" {
+        let limit = args[2].parse::<u32>();
+        match limit {
+            Ok(s) => options.limit = s,
+            Err(_) => {
+                eprintln!("Error: Expected value after limit flag to be a positive whole number");
+                eprintln!("enter '{} --help' to learn more", file_name);
+                process::exit(1)
+            }
+        }
+    } else if arg == "/r" || arg == "--regex" {
+        let regex = Regex::new(&args[2]);
+        match regex {
+            Ok(s) => options.regex = Some(s),
+            Err(_) => {
+                eprintln!(
+                    "Error: Expected expression after regex flag to be a valid regular expression"
+                );
+                eprintln!("enter '{} --help' to learn more", file_name);
+                process::exit(1)
+            }
+        }
+    }
+}
+
+fn find_file(search_term: &str, options: &Options) {
+    let path = ".";
+    let mut files = Vec::new();
+
+    for entry in WalkDir::new(path) {
+        let entry = match entry {
+            Ok(e) => e,         // Successfully retrieve entry
+            Err(_) => continue, // Skip entries that cannot be accessed
+        };
+
+        let metadata = match entry.metadata() {
+            Ok(meta) => meta,   // Successfully retrieve metadata
+            Err(_) => continue, // Skip files with inaccessible metadata
+        };
+
+        if metadata.is_file() && matches_search(entry.path(), search_term, options) {
+            files.push(entry.path().display().to_string());
+        }
+    }
+
+    if !options.count {
+        for (idx, file) in files.clone().into_iter().enumerate() {
+            if idx as u32 == options.limit {
+                break;
+            }
+
+            // Highlight the matched part
+            let highlighted = file.replace(
+                search_term,
+                &format!("\x1b[32m{}\x1b[0m", search_term), // Green ANSI escape codes
+            );
+            println!("{}", highlighted);
+        }
+    }
+
+    println!("\n{} results found", files.len());
+}
+
+fn matches_search(path: &std::path::Path, search_term: &str, options: &Options) -> bool {
+    if let Some(file_name) = path.file_name().and_then(|name| name.to_str()) {
+        if options.base_name {
+            // Case-insensitive search in base name
+            file_name
+                .to_lowercase()
+                .contains(&search_term.to_lowercase())
+        } else if options.case_sens {
+            // Case-sensitive search in the full path
+            path.display().to_string().contains(search_term)
+        } else if options.regex.is_some() {
+            // search with regex
+            options
+                .regex
+                .clone()
+                .unwrap()
+                .is_match(&path.display().to_string().to_lowercase())
+        } else {
+            // Case-insensitive search in full path
+            path.display()
+                .to_string()
+                .to_lowercase()
+                .contains(&search_term.to_lowercase())
+        }
+    } else {
+        false
+    }
+}
---
+++ touch/src/main.rs
@@ -0,0 +1,152 @@
+use filetime::FileTime;
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::process;
+use std::time::SystemTime;
+
+struct Options {
+    no_create: bool, // Prevent file creation if it doesn't exist
+    directory: bool, // Create directories instead of files
+    acc_time: bool,  // Update only access time
+    mod_time: bool,  // Update only modification time
+}
+
+impl Options {
+    fn new() -> Self {
+        Options {
+            no_create: false,
+            directory: false,
+            acc_time: false,
+            mod_time: false,
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect(); // Collect command-line arguments
+    let path = env::current_exe()?; // Get the path of the currently running program
+    let file_name_exe = path.file_name().unwrap().to_string_lossy(); // Extract the executable name
+    let file_name = path.file_stem().unwrap().to_string_lossy(); // Extract the file name without extension
+
+    let arguments = [
+        "/h",
+        "--help",
+        "/c",
+        "--no-create",
+        "/d",
+        "--directory",
+        "/a",
+        "--access-time",
+        "/m",
+        "--modification-time",
+    ]; // Recognized arguments
+    let mut options = Options::new(); // Initialize options
+
+    check_args(&args, &file_name, &mut options); // Parse and set options based on args passed in
+
+    for arg in args.iter() {
+        if (arg == &file_name.to_string() || arg == &file_name_exe.to_string())
+            || (arguments.contains(&arg.as_str()))
+        // assume user does not want to name a file/folder one of the arguments
+        {
+            continue; // Skip the executable name and recognized arguments
+        }
+
+        make_file(arg, &options); // Process file creation or updates
+    }
+
+    Ok(())
+}
+
+// Parse command-line arguments and set options accordingly
+fn check_args(args: &[String], file_name: &str, options: &mut Options) {
+    if args.len() == 1 {
+        eprintln!("Usage: {}[.exe] [args] <file_name>", file_name);
+        eprintln!("enter '{} --help' to learn more", file_name);
+        process::exit(1);
+    }
+
+    let arg = &args[1];
+
+    if arg == "/h" || arg == "--help" {
+        // Display help information
+        println!("Usage: {}[.exe] [args] <file_name>", file_name);
+        println!("\nOPTIONAL ARGUMENTS\n");
+        println!("Note: Only one argument can be used at a time\n");
+        println!("/h or --help                Displays this help message\n");
+        println!("/c or --no-create           Prevents creating new files if they don't already exist\n                            If a file exists, the timestamps will be updated\n                            but if the file doesn't exist, nothing will happen\n");
+        println!("\n/d or --directory           Creates directories instead of files\n                            Can also create nested folders:\n                            {} --directory this/is/a/nested/folder\n", file_name);
+        println!("/a or --access-time         Only updates the accessed time of the file if the file already exists,\n                            otherwise it creates the file like normal\n");
+        println!("\n/m or --modification-time   Only updates the modified time of the file if the file already exists,\n                            otherwise it creates the file like normal\n");
+        process::exit(0);
+    } else if arg == "/c" || arg == "--no-create" {
+        options.no_create = true;
+    } else if arg == "/d" || arg == "--directory" {
+        options.directory = true;
+    } else if arg == "/a" || arg == "--access-time" {
+        options.acc_time = true;
+    } else if arg == "/m" || arg == "--modification-time" {
+        options.mod_time = true;
+    }
+}
+
+// Create or update a file or directory based on the provided options
+fn make_file(file_name: &String, options: &Options) {
+    if options.no_create && !std::path::Path::new(file_name).exists() {
+        // Skip creating the file if --no-create is specified and it doesn't exist
+    } else if options.directory {
+        match fs::create_dir_all(file_name) {
+            Ok(_) => {}
+            Err(e) if e.kind() == io::ErrorKind::PermissionDenied => {
+                eprintln!(
+                    "Error: Unable to create the folder '{}'.\nPossible reasons:\n - Insufficient permissions.\n - Invalid folder name.",
+                    file_name
+                );
+                process::exit(1);
+            }
+            Err(e) => {
+                eprintln!("An unexpected error occurred: {}", e);
+                process::exit(1);
+            }
+        }
+    } else if options.acc_time {
+        create_file(file_name, "a"); // Update access time only
+    } else if options.mod_time {
+        create_file(file_name, "m"); // Update modification time only
+    } else {
+        create_file(file_name, "none"); // Create or update both timestamps
+    }
+}
+
+// Handle file creation and timestamp updates
+fn create_file(file_name: &String, type_: &str) {
+    match File::create_new(file_name) {
+        Ok(_) => {}
+        Err(e) if e.kind() == io::ErrorKind::PermissionDenied => {
+            eprintln!(
+                "Error: Unable to create the file '{}'.\nPossible reasons:\n - Insufficient permissions.\n - Invalid file name.",
+                file_name
+            );
+            process::exit(1);
+        }
+        Err(e) if e.kind() == io::ErrorKind::AlreadyExists => {
+            let now = SystemTime::now();
+            let now_filetime = FileTime::from_system_time(now);
+
+            if type_ == "a" {
+                filetime::set_file_atime(file_name, now_filetime).unwrap(); // Update access time
+            } else if type_ == "m" {
+                filetime::set_file_mtime(file_name, now_filetime).unwrap(); // Update modification time
+            } else {
+                // Update both
+                filetime::set_file_times(file_name, now_filetime, now_filetime).unwrap();
+            }
+        }
+        Err(e) => {
+            eprintln!("An unexpected error occurred: {}", e);
+            process::exit(1);
+        }
+    }
+}
